747576041d11c5e45005dd35e123b0442a4a8bd5
JuniorCriste <informaticode@gmail.com>
2026-08-03 01:35:49 -0300

    1.1

--- app.js
+++ app.js
@@ -280,7 +280,7 @@ function reproduzirAudioHora() {
     // Converte de formato 24h para 12h (0h e 12h viram 12; 1h e 13h viram 1, etc.)
     const horaFormatada = (horas24 % 12) === 0 ? 12 : (horas24 % 12);
     
-    const audioHora = new Audio(`sounds/hora/${horaFormatada}.ogg`);
+    const audioHora = new Audio(`voice/hora/${horaFormatada}.ogg`);
     audioHora.play().catch(() => console.log(`Erro ao reproduzir o arquivo sounds/hora/${horaFormatada}.ogg`));
 }
 
